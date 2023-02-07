6de05236058c98411fa965d8bda7014ee72ef067
Felipe Carvalho <felipehmc14@gmail.com>
2023-02-07 14:55:08 -0300

    Commit inicial

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Heading = () => {
+    return <div className="text-center text-4xl font-bold">
+        To Do List - Redux
+    </div>
+};
+
+export default Heading;
